38e62f238f934c775a7277da0f86516f5ec1073a
Aaryan Soni <121357513+aaryann20@users.noreply.github.com>
2024-05-31 23:41:07 +0530

    Add files via upload

---
+++ geminiapi.py
@@ -0,0 +1,16 @@
+#-!pip install google-generativeai
+
+import google.generativeai as genai
+GOOGLE_API_KEY = "AIzaSyCfq9PeM1zIFUyLh8we8OqPkwzmU_-PiGs"
+
+
+user_text1="""
+give me hindi translation of this sentance
+"hey! you navneet ai boy "
+"""
+genai.configure(api_key= GOOGLE_API_KEY)
+model = genai.GenerativeModel("gemini-pro")
+response1= model.generate_content(user_text1)
+result1 = response1.text
+
+print(result1)
